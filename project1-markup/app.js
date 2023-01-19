6f8c91baca43db850a59b648d49995b42f90e249
Michelle-Wanderi <michellewangari12@gmail.com>
2023-01-19 14:46:47 +0300

    added

--- project1-markup/app.js
+++ project1-markup/app.js
@@ -1,23 +1,57 @@
-const page = (
-    <div>
-        <img src="./react.png" width = "40px"></img>
-        <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first created in 2013</li>
-                <li>Has originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps including mobile apps</li>
-            </ul>
-    </div>
-)
-ReactDOM.render(
-    page ,
-    document.getElementById("root")
-)
+// const page = (
+//     <div>
+//         <img src="./react.png" width = "40px"></img>
+//         <h1>Fun facts about React</h1>
+//             <ul>
+//                 <li>Was first created in 2013</li>
+//                 <li>Has originally created by Jordan Walke</li>
+//                 <li>Has well over 100K stars on Github</li>
+//                 <li>Is maintained by Facebook</li>
+//                 <li>Powers thousands of enterprise apps including mobile apps</li>
+//             </ul>
+//     </div>
+// )
+// ReactDOM.render(
+//     page ,
+//     document.getElementById("root")
+// )
 
 //This method of storing all JSX elements in one variable is not common nor is it efficient
 
 //Creating custom components
 
- 
+//A component is a function that returns React elements whenever the function is called.
+//A component must be named using Camelcase and it is not called as function where we use () instead we use <Temporary /> which are known as angle brackets
+
+//  function TemporaryName() {
+//     return (
+
+//         <div>
+//             <img src="./react.png" width = "40px"></img>
+//             <h1>Fun facts about React</h1>
+//                 <ul>
+//                 <li>Was first created in 2013</li>
+//                 <li>Has originally created by Jordan Walke</li>
+//                 <li>Has well over 100K stars on Github</li>
+//                 <li>Is maintained by Facebook</li>
+//                 <li>Powers thousands of enterprise apps including mobile apps</li>
+//             </ul>
+//     </div>
+//     )
+//  }
+//  ReactDOM.render(<TemporaryName/>, document.getElementById("root"))
+
+
+function Page() {
+    return (
+        <div>
+            <h1> Reasons why I love React</h1>
+            <ol>
+                <li>It is efficient</li>
+                <li>Easy to learn</li>
+                <li>It is a hireable skill</li>
+            </ol>
+        </div>
+    )
+}
+ReactDOM.render( <Page /> , document.getElementById("root"))
